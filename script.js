d787b94a09885095c0ff30c43abf740870b188d5
michelle0li <69157861+michelle0li@users.noreply.github.com>
2021-01-23 09:31:31 -0500

    Sample Solution

---
+++ script.js
@@ -0,0 +1,16 @@
+
+//var title = document.getElementById("title"),
+    //solution = document.getElementById("solution");
+
+//title.onmouseover = function(){
+    //solution.style.display = "block";
+//}
+
+//title.onmouseout = function(){
+    //solution.style.display = "none";
+//}
+
+//function changeText() {
+    //title.innerHTML = (title.innerHTML == "title" ) ?
+    //"solution" : "title"
+//}
